a0b1cc79f570ef7659427c3ec16d28ef4508f8c6
“adiletD” <daniiarov.adilet@gmail.com>
2022-04-19 12:29:08 +0800

    register user added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,18 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
+userSchema.pre('save', async function (next) {
+  // but when the profile is updated we shuoldnot make it run
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  // to hash password asyncronously
+  const salt = await bcrypt.genSalt(10)
+  // resetting plan text to ashed password
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
