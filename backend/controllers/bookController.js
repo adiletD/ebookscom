66ff0d3d6380c5c93d4911f67bd7c299b77f8afd
“adiletD” <daniiarov.adilet@gmail.com>
2022-04-20 14:49:49 +0800

    create and delete product by admin added

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -26,4 +26,53 @@ const getBookById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getBooks, getBookById }
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteBook = asyncHandler(async (req, res) => {
+  const book = await Book.findById(req.params.id)
+
+  if (book) {
+    await book.remove()
+    res.json({ message: 'Book removed' })
+  } else {
+    res.status(404)
+    throw new Error('Book not found')
+  }
+})
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createBook = asyncHandler(async (req, res) => {
+  const { name, price, description, image, genre, countInStock } = req.body
+
+  // const product = new Book({
+  //   name: 'Sample name',
+  //   price: 0,
+  //   user: req.user._id,
+  //   image: '/images/sample.jpg',
+  //   brand: 'Sample brand',
+  //   category: 'Sample category',
+  //   countInStock: 0,
+  //   numReviews: 0,
+  //   description: 'Sample description',
+  // })
+
+  const book = new Book({
+    name: name,
+    price: price,
+    user: req.user._id,
+    description: description,
+    image: image,
+    genre: genre,
+    countInStock: countInStock,
+    description: description,
+  })
+
+  const createdBook = await book.save()
+
+  res.status(201).json(createdBook)
+})
+
+export { getBooks, getBookById, deleteBook, createBook }
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,9 +1,18 @@
 import express from 'express'
-import { getBooks, getBookById } from '../controllers/bookController.js'
+import {
+  getBooks,
+  getBookById,
+  deleteBook,
+  createBook,
+} from '../controllers/bookController.js'
+
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/').get(getBooks)
 router.route('/:id').get(getBookById)
+router.route('/createBook').post(protect, admin, createBook)
+router.route('/deleteBook/:id').delete(protect, admin, deleteBook)
 
 export default router
